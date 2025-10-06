32f23300b29e293abb694da70b3039222e0a4519
molnar13 <marcell.13.molnar@gmail.com>
2025-10-06 09:51:32 +0200

    new endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,22 @@
+<?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PostalCodeController;
 
+// Publikus végpontok - nincs hitelesítés
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+// Publikus postal codes végpontok
+Route::get('/postal-codes', [PostalCodeController::class, 'index']);
+Route::get('/postal-codes/{postalCode}', [PostalCodeController::class, 'show']);
+Route::get('/postal-codes/search/{code}', [PostalCodeController::class, 'searchByCode']);
+
+// Védett végpontok - hitelesítés szükséges
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    
-    // Védett végpontok
     Route::post('/postal-codes', [PostalCodeController::class, 'store']);
     Route::put('/postal-codes/{postalCode}', [PostalCodeController::class, 'update']);
     Route::delete('/postal-codes/{postalCode}', [PostalCodeController::class, 'destroy']);
 });
-
-// Publikus végpontok
-Route::get('/postal-codes', [PostalCodeController::class, 'index']);
-Route::get('/postal-codes/{postalCode}', [PostalCodeController::class, 'show']);
-Route::get('/postal-codes/search/{code}', [PostalCodeController::class, 'searchByCode']);
